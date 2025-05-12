2a42ef961f9b22ca5ab96f2c6156e7ef79d50c20
naveen g <navindian2024@gmail.com>
2025-05-12 15:34:43 +0530

    form
    completed all the controls in the form.

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -5,7 +5,16 @@ class SignUpPage extends React.Component {
     return (
       <div>
         <h1>Sign Up</h1>
-        <input/>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" />
+        <label htmlFor="password">Password</label>
+        <input id="password" type="password" />
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input id="confirmPassword" type="password" />
+        <button type="submit">Sign Up</button>  
+
       </div>
     );  
 }  
--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -8,6 +8,31 @@ describe("SignUpPage", () => {
     const header = screen.getByRole("heading", { name: "Sign Up" });
     expect(header).toBeInTheDocument();
   });
- 
+  it("has username input", () => {
+    render(<SignUpPage />);
+    const input = screen.getByRole("textbox", { name: "Username" });
+    expect(input).toBeInTheDocument();
+  });
+ it("has email input", () => {
+    render(<SignUpPage />);
+    const input = screen.getByLabelText("Email");
+    expect(input).toBeInTheDocument();
+  });
+  it("has password input", () => {
+    render(<SignUpPage />);
+    const input = screen.getByLabelText("Password");
+    expect(input).toBeInTheDocument();
+  });
+  it("has password repeat input", () => {
+    render(<SignUpPage />);
+    const input = screen.getByLabelText("Confirm Password");
+    expect(input).toBeInTheDocument();
+  });
+  it("has submit button", () => {
+    render(<SignUpPage />);
+    const button = screen.getByRole("button", { name: "Sign Up" });
+    expect(button).toBeInTheDocument();
+  });
+  
 });
 });
